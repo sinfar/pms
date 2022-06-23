5adfd2ea3b05f27f6560fa018bc8f6f4ef25497e
江新发 <1028722879@qq.com>
2022-06-23 22:51:44 +0800

    更新 deliver.sh

--- deliver.sh
+++ deliver.sh
@@ -21,4 +21,5 @@ VERSION=`mvn help:evaluate -Dexpression=project.version | grep "^[^\[]"`
 echo 'The following command runs and outputs the execution of your Java'
 echo 'application (which Jenkins built using Maven) to the Jenkins UI.'
 //set -x
-java -jar target/${NAME}-${VERSION}.jar
+cd start
+java -jar target/start-${VERSION}.jar jxf.pms.Application
