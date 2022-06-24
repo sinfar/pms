6227be850e9425f0251e6d6fba3c703ad79f3b77
江新发 <1028722879@qq.com>
2022-06-24 09:35:21 +0800

    更新 deliver.sh

--- deliver.sh
+++ deliver.sh
@@ -17,5 +17,6 @@ echo 'The following command runs and outputs the execution of your Java'
 echo 'application (which Jenkins built using Maven) to the Jenkins UI.'
 
 dir("start") {
+    pwd
     java -jar target/start-${VERSION}.jar
 }
